addf6c894dd82e86495aea7d5ef486558e643f8f
Andreas Bresser <self@andreasbresser.de>
2020-02-09 01:45:08 +0100

    First version without scriptiong

---
+++ Assets/Scripts/PivotCenterGizmo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PivotCenterGizmo : MonoBehaviour
+{
+    public int pixelToCoords = 1024;
+    public Sprite sprite;
+    // Start is called before the first frame update
+
+    void Awake() {
+        OnValidate();
+    }
+
+    void OnValidate() {
+        GetSprite();
+    }
+    bool GetSprite()
+    {
+        if (!sprite) {
+            sprite = GetComponent<SpriteRenderer>().sprite;
+        }
+        return (bool)(sprite != null);
+    }
+
+    // Update is called once per frame
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+
+        //Debug.Log("Pivot " + sprite.pivot);
+        //Debug.Log("Rect " + sprite.rect);
+
+        Gizmos.DrawLine(
+            transform.position + Vector3.left,
+            transform.position + Vector3.right
+        );
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Test Kenny Assets with Unity Tilemaps
+A simple test scene for experiments showing some how Sorting Groups and Z-Position for tilemaps work (or not work) and and a script that re-calculates the tile dynamically
+
+Some other notes on working with tilemaps:
+
+- make sure all GameObjects are scalled to 1 1 1, if you like to change the Scale change it at the Sprite > Pixel per Unit value (better: scale the image in some other software).
+- make sure the Pivit-Point is at the bottom of your Character and placed correctly on your Tilemaps.
