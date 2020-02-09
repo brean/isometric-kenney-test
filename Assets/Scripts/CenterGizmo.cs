9a86a1a5bb066f75ddf51a9ddf1dbb3503572697
Andreas Bresser <self@andreasbresser.de>
2020-02-09 10:44:08 +0100

    Screenshots and Working solution without tilemaps

---
+++ Assets/Scripts/CenterGizmo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * draw a line at the object center (shoule be the pivot center as well)
+ */
+[ExecuteInEditMode]
+public class CenterGizmo : MonoBehaviour
+{
+    public Color color = Color.magenta;
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+
+        Gizmos.DrawLine(
+            transform.position + Vector3.left,
+            transform.position + Vector3.right
+        );
+    }
+    
+}
